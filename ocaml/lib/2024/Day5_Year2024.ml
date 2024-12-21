f4ab74ab8a3f991f1b8d7dc4e95f44c01a359968
Davide Cioni <88600810+deivshon@users.noreply.github.com>
2024-12-21 17:08:22 +0100

    feat: ✨ add ocaml day 5 year 2024 second solution

--- ocaml/lib/2024/Day5_Year2024.ml
+++ ocaml/lib/2024/Day5_Year2024.ml
@@ -27,25 +27,42 @@ let parse_puzzle_input raw_input =
   let updates = parse_updates raw_updates in
   (orderings, updates)
 
-let is_valid_update orderings update =
-  let ordering_invalidates_update update ordering =
-    let rec inner update snd_seen =
-      match update with
-      | [] -> false
-      | x :: xs ->
-          if x = fst ordering && snd_seen then true
-          else (inner [@tailcall]) xs (snd_seen || x = snd ordering)
-    in
-    inner update false
+let get_relevant_orderings orderings update =
+  let step acc o =
+    if List.mem (fst o) update && List.mem (snd o) update then o :: acc else acc
   in
-  not (List.exists (ordering_invalidates_update update) orderings)
+  List.fold_left step [] orderings |> List.rev
+
+let pages_compare orderings p1 p2 =
+  if List.mem (p1, p2) orderings then -1
+  else if List.mem (p2, p1) orderings then 1
+  else 0
+
+let is_valid_update orderings update =
+  let ordered = List.sort (pages_compare orderings) update in
+  List.for_all2 ( = ) update ordered
+
+let take_middle l = List.nth l (List.length l / 2)
 
 let first_solution raw_input =
-  let take_middle l = List.nth l (List.length l / 2) in
   let orderings, updates = parse_puzzle_input raw_input in
   let valid_updates = List.filter (is_valid_update orderings) updates in
   List.map take_middle valid_updates
-  |> List.fold_left (fun acc v -> acc + v) 0
+  |> List.fold_left ( + ) 0
+  |> string_of_int
+  |> Result.ok
+
+let second_solution raw_input =
+  let take_middle l = List.nth l (List.length l / 2) in
+  let orderings, updates = parse_puzzle_input raw_input in
+  let invalid_updates =
+    let filter_fn update = not (is_valid_update orderings update) in
+    List.filter filter_fn updates
+  in
+  invalid_updates
+  |> List.map (List.sort (pages_compare orderings))
+  |> List.map take_middle
+  |> List.fold_left ( + ) 0
   |> string_of_int
   |> Result.ok
 
@@ -57,5 +74,12 @@ let (solutions : Execution.solution list) =
       year = 2024;
       solution_number = 1;
       expected_solution = Some "6242";
+    };
+    {
+      solution_fn = second_solution;
+      day = 5;
+      year = 2024;
+      solution_number = 2;
+      expected_solution = Some "5169";
     };
   ]
