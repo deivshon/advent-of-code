a64b18ce2c8c6174fec7f2a42f6b5c68b7c2f379
Davide Cioni <88600810+deivshon@users.noreply.github.com>
2024-12-22 16:07:57 +0100

    feat: ✨ always flush stdout after printing solution

--- ocaml/lib/Execution.ml
+++ ocaml/lib/Execution.ml
@@ -13,26 +13,30 @@ let date_of execution =
     (string_of_int execution.solution_number)
 
 let execute_solution execution =
-  try
-    let execution_date = date_of execution in
-    let puzzle_input =
-      Utils.get_puzzle_input_lines ~year:execution.year ~day:execution.day
-    in
-    let solution = execution.solution_fn puzzle_input in
-    match solution with
-    | Ok v -> (
-        match execution.expected_solution with
-        | Some expected ->
-            if v = expected then Printf.printf "%s: %s\n" execution_date v
-            else
-              Printf.printf "%s: %s !!! (expected %s)\n" execution_date v
-                expected
-        | None ->
-            Printf.printf "%s: %s ??? (no expected solution specified)\n"
-              execution_date v)
-    | Error err ->
-        Printf.printf "%s: could not obtain solution: %s\n" execution_date err
-  with exc ->
-    let execution_date = date_of execution in
-    Printf.printf "%s: errored with exception: %s\n" execution_date
-      (Printexc.to_string exc)
+  let execute =
+    try
+      let execution_date = date_of execution in
+      let puzzle_input =
+        Utils.get_puzzle_input_lines ~year:execution.year ~day:execution.day
+      in
+      let solution = execution.solution_fn puzzle_input in
+      match solution with
+      | Ok v -> (
+          match execution.expected_solution with
+          | Some expected ->
+              if v = expected then Printf.printf "%s: %s\n" execution_date v
+              else
+                Printf.printf "%s: %s !!! (expected %s)\n" execution_date v
+                  expected
+          | None ->
+              Printf.printf "%s: %s ??? (no expected solution specified)\n"
+                execution_date v)
+      | Error err ->
+          Printf.printf "%s: could not obtain solution: %s\n" execution_date err
+    with exc ->
+      let execution_date = date_of execution in
+      Printf.printf "%s: errored with exception: %s\n" execution_date
+        (Printexc.to_string exc)
+  in
+  execute;
+  flush stdout
