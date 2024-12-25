632edb56b27135fe503d15d3d5fb4c20a0bb2fba
Davide Cioni <88600810+deivshon@users.noreply.github.com>
2024-12-25 18:57:04 +0100

    refactor(ocaml): ♻️ fix levels naming in day 2 year 2024

--- ocaml/lib/2024/Day2_Year2024.ml
+++ ocaml/lib/2024/Day2_Year2024.ml
@@ -12,19 +12,19 @@ let is_valid_report_part1 report =
     match levels with
     | [] -> true
     | [ _ ] -> true
-    | ls1 :: ls2 :: rest -> (
+    | l1 :: l2 :: rest -> (
         let current_direction =
-          if ls1 - ls2 > 0 then Increasing else Decreasing
+          if l1 - l2 > 0 then Increasing else Decreasing
         in
-        let distance = Int.abs (ls1 - ls2) in
+        let distance = Int.abs (l1 - l2) in
         let distance_exceeded = distance > 3 || distance = 0 in
         if distance_exceeded then false
         else
           match previous_direction with
-          | None -> (inner [@tailcall]) (ls2 :: rest) (Some current_direction)
+          | None -> (inner [@tailcall]) (l2 :: rest) (Some current_direction)
           | Some d ->
               if current_direction <> d then false
-              else (inner [@tailcall]) (ls2 :: rest) previous_direction)
+              else (inner [@tailcall]) (l2 :: rest) previous_direction)
   in
   inner report None
 
