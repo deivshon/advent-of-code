0aba6b251f40ae0a1d101659feaa6cd565d02478
Davide Cioni <88600810+deivshon@users.noreply.github.com>
2025-05-03 17:01:32 +0200

    refactor(rust): ♻️ cli tweaks

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,7 +2,7 @@ pub mod ext;
 pub mod problems;
 pub mod year_2020;
 
-use std::time::SystemTime;
+use std::time::{Duration, SystemTime};
 use std::{fs, path::Path, process};
 
 use clap::Parser;
@@ -20,15 +20,34 @@ struct Arguments {
     part: Option<i32>,
 }
 
-fn filter_from_option<T: PartialEq + 'static>(value: Option<T>) -> Box<dyn Fn(T) -> bool> {
-    match value {
-        None => Box::new(|_| true),
-        Some(value) => Box::new(move |compared| value == compared),
+fn filter_from_option<T: PartialEq + 'static>(value: Option<T>) -> impl Fn(T) -> bool {
+    move |target| match &value {
+        None => true,
+        Some(value) => target == *value,
+    }
+}
+
+fn prettify_duration(value: Duration) -> String {
+    if value.as_micros() < 2500 {
+        format!("{}μs", value.as_micros())
+    } else if value.as_millis() < 2500 {
+        format!("{}ms", value.as_millis())
+    } else {
+        format!("{}s", value.as_secs())
     }
 }
 
 fn main() {
     let args = Arguments::parse();
+
+    if args.part.is_some() && args.day.is_none() {
+        eprintln!("must specify day");
+        process::exit(1);
+    }
+    if args.day.is_some() && args.year.is_none() {
+        eprintln!("must specify year");
+        process::exit(1);
+    }
 
     let matches_year = filter_from_option(args.year);
     let matches_day = filter_from_option(args.day);
@@ -42,10 +61,11 @@ fn main() {
         .collect::<Vec<&Solution>>();
 
     if wanted_solutions.is_empty() {
-        eprintln!("No solutions found for the given criteria.");
+        eprintln!("no solutions found for the given criteria.");
         process::exit(1)
     }
 
+    let mut total_time = Duration::new(0, 0);
     wanted_solutions.iter().for_each(|solution| {
         let input_file_path = format!("./../puzzle-inputs/{}/{}.txt", solution.year, solution.day);
         let input_file = Path::new(&input_file_path);
@@ -72,6 +92,7 @@ fn main() {
                 return;
             }
         };
+        total_time += time_taken;
 
         let correctness_indicator = match (&result, solution.expected) {
             (Ok(result), Some(expected)) => {
@@ -82,16 +103,6 @@ fn main() {
                 }
             }
             (_, _) => String::from("❓"),
-        };
-
-        let time_taken_pretty = {
-            if time_taken.as_micros() < 2500 {
-                format!("{}μs", time_taken.as_micros())
-            } else if time_taken.as_millis() < 2500 {
-                format!("{}ms", time_taken.as_millis())
-            } else {
-                format!("{}s", time_taken.as_secs())
-            }
         };
 
         let expected_string: Option<String> = match (&result, solution.expected) {
@@ -111,12 +122,16 @@ fn main() {
             solution.year,
             solution.day,
             solution.part,
-            time_taken_pretty,
+            prettify_duration(time_taken),
             match result {
                 Ok(s) => s,
                 Err(e) => format!("error computing solution: {}", e),
             },
             expected_string.unwrap_or_default()
-        )
+        );
     });
+
+    if wanted_solutions.len() > 1 {
+        println!("\n🕑 | {}", prettify_duration(total_time));
+    }
 }
